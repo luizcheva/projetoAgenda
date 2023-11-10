9e79e5f8c106cce68a7ab1ef946f60a9542884da
Luiz Cheva <luiz-cheva@hotmail.com>
2023-11-10 15:38:26 -0300

    Inserindo Partials

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -13,6 +13,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -30,8 +31,11 @@ def contact(request, contact_id):
         show=True,
     )
 
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
