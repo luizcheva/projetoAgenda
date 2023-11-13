a1d6a7977018b251f4709e7c675b85dc54059aa0
Luiz Cheva <luiz-cheva@hotmail.com>
2023-11-13 09:52:33 -0300

    Utilizando request para pegar o valor do GET na busca

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -63,7 +63,8 @@ def search(request):
 
     context = {
         'contacts': contacts,
-        'site_title': 'Contatos - ',
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
