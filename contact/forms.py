661904a39e2d0d9003aeb90dc3b53b6cdda73571
Luiz Cheva <luiz-cheva@hotmail.com>
2023-11-21 12:11:20 -0300

    Inserindo as imagens de contact

--- contact/forms.py
+++ contact/forms.py
@@ -5,24 +5,13 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a classe-b',
-                'placeholder': 'Escreva aqui com aa'
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro nome',
-        help_text='Ajuda para o usuario'
+        )
     )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Escreva aqui com __init__'
-        # })
 
     class Meta:
         model = models.Contact
@@ -33,15 +22,8 @@ class ContactForm(forms.ModelForm):
             'email',
             'description',
             'category',
+            'picture',
         )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui'
-        #         }
-        #     )
-        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
